3aadbb313958bf1958ee2e0e223d8859b2fa46f5
Paul Lam <paul@quantisan.com>
2025-06-25 11:21:16 +0900

    Squash merge feat/renderer-and-preload into main

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -1,10 +1,12 @@
 (ns main.core
-  (:require ["electron/main" :refer [app BrowserWindow]]))
+  (:require ["electron/main" :refer [app BrowserWindow]]
+            ["path" :as path]))
 
 (defn create-window []
   (let [win (BrowserWindow.
               (clj->js {:width 800
-                        :height 600}))]
+                        :height 600
+                        :webPreferences {:preload (.join path js/__dirname "../resources/public/js/preload.js")}}))]
     (.loadFile win "resources/public/index.html")))
 
 (defn main []
