7386dce168f5bede2f5a157170befe9403976b5b
Paul Lam <paul@quantisan.com>
2025-06-26 15:18:22 +0900

    Refactor/replicant renderer (#1)
    * feat: add Replicant renderer with version info display
    
    * chore: update .gitignore to ignore compiled JavaScript files
    
    * chore: update clean script to remove compiled JS directory
    
    * fix: add 'unsafe-eval' to CSP to support ClojureScript runtime
    
    * fix: update electron import path in preload script
    
    * feat: add diagnostic prints to start! function for version debugging
    
    * feat: add diagnostic logging to preload script for contextBridge
    
    * fix: update renderer and preload script to correctly access versions API
    
    * refactor: remove debug logging and simplify version API handling
    
    * refactor: extract version retrieval logic into separate function
    
    * fix: correct method call syntax in get-versions function

---
+++ src/renderer/core.cljs
@@ -0,0 +1,19 @@
+(ns renderer.core
+  (:require [replicant.dom :as r]))
+
+(defn version-info [versions]
+  [:div#info
+   "This app is using Chrome (v" (:chrome versions) "), "
+   "Node.js (v" (:node versions) "), "
+   "and Electron (v" (:electron versions) ")"])
+
+(defn get-versions []
+  (when-let [versions-api (.-versions js/window)]
+    {:chrome   (.chrome versions-api)
+     :node     (.node versions-api)
+     :electron (.electron versions-api)}))
+
+(defn start! []
+  (when-let [versions (get-versions)]
+    (r/render (js/document.getElementById "info")
+              (version-info versions))))
